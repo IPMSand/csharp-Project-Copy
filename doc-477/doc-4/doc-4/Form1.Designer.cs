4c069d41ac8fca75e37809cb34ced0ee919b2322
IPMSand <131543563+IPMSand@users.noreply.github.com>
2024-04-10 10:13:04 +0530

    Save my doc 4 eddit first commit

---
+++ doc-477/doc-4/doc-4/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace doc_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblRegister = new System.Windows.Forms.Label();
+            this.lblLoginBOX = new System.Windows.Forms.Label();
+            this.lblAlert = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.butClear = new System.Windows.Forms.Button();
+            this.butLogin = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblForgotPass = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lblTitle.Font = new System.Drawing.Font("Jokerman", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(46, 138);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(316, 55);
+            this.lblTitle.TabIndex = 36;
+            this.lblTitle.Text = "CARELINK EHR";
+            // 
+            // lblRegister
+            // 
+            this.lblRegister.AutoSize = true;
+            this.lblRegister.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegister.Location = new System.Drawing.Point(90, 497);
+            this.lblRegister.Name = "lblRegister";
+            this.lblRegister.Size = new System.Drawing.Size(191, 24);
+            this.lblRegister.TabIndex = 35;
+            this.lblRegister.Text = "Sign up/Resister?";
+            this.lblRegister.Click += new System.EventHandler(this.lblRegister_Click);
+            // 
+            // lblLoginBOX
+            // 
+            this.lblLoginBOX.AutoSize = true;
+            this.lblLoginBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoginBOX.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblLoginBOX.Location = new System.Drawing.Point(451, 38);
+            this.lblLoginBOX.Name = "lblLoginBOX";
+            this.lblLoginBOX.Size = new System.Drawing.Size(101, 38);
+            this.lblLoginBOX.TabIndex = 33;
+            this.lblLoginBOX.Text = "Login";
+            // 
+            // lblAlert
+            // 
+            this.lblAlert.AutoSize = true;
+            this.lblAlert.Location = new System.Drawing.Point(812, 43);
+            this.lblAlert.Name = "lblAlert";
+            this.lblAlert.Size = new System.Drawing.Size(44, 16);
+            this.lblAlert.TabIndex = 32;
+            this.lblAlert.Text = "label3";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblPassword.Location = new System.Drawing.Point(502, 213);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(128, 27);
+            this.lblPassword.TabIndex = 31;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblUsername.Location = new System.Drawing.Point(496, 82);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(134, 27);
+            this.lblUsername.TabIndex = 30;
+            this.lblUsername.Text = "Username";
+            this.lblUsername.Click += new System.EventHandler(this.lblUsername_Click);
+            // 
+            // butClear
+            // 
+            this.butClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.butClear.Font = new System.Drawing.Font("Georgia", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butClear.Location = new System.Drawing.Point(501, 462);
+            this.butClear.Name = "butClear";
+            this.butClear.Size = new System.Drawing.Size(460, 46);
+            this.butClear.TabIndex = 29;
+            this.butClear.Text = "CLEAR";
+            this.butClear.UseVisualStyleBackColor = false;
+            this.butClear.Click += new System.EventHandler(this.butClear_Click);
+            // 
+            // butLogin
+            // 
+            this.butLogin.BackColor = System.Drawing.Color.Teal;
+            this.butLogin.Font = new System.Drawing.Font("Georgia", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butLogin.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.butLogin.Location = new System.Drawing.Point(501, 343);
+            this.butLogin.Name = "butLogin";
+            this.butLogin.Size = new System.Drawing.Size(460, 113);
+            this.butLogin.TabIndex = 28;
+            this.butLogin.Text = "LOGIN";
+            this.butLogin.UseVisualStyleBackColor = false;
+            this.butLogin.Click += new System.EventHandler(this.butLogin_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtPassword.Location = new System.Drawing.Point(501, 243);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(461, 81);
+            this.txtPassword.TabIndex = 27;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtUsername.Location = new System.Drawing.Point(500, 112);
+            this.txtUsername.Multiline = true;
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(461, 81);
+            this.txtUsername.TabIndex = 26;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(-8, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(432, 613);
+            this.pictureBox1.TabIndex = 25;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblForgotPass
+            // 
+            this.lblForgotPass.AutoSize = true;
+            this.lblForgotPass.Font = new System.Drawing.Font("Georgia", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblForgotPass.Location = new System.Drawing.Point(508, 539);
+            this.lblForgotPass.Name = "lblForgotPass";
+            this.lblForgotPass.Size = new System.Drawing.Size(199, 24);
+            this.lblForgotPass.TabIndex = 37;
+            this.lblForgotPass.Text = "Forgot password?";
+            this.lblForgotPass.Click += new System.EventHandler(this.lblForgotPass_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 579);
+            this.Controls.Add(this.lblForgotPass);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblRegister);
+            this.Controls.Add(this.lblLoginBOX);
+            this.Controls.Add(this.lblAlert);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.butClear);
+            this.Controls.Add(this.butLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "CARELINK EHR";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblRegister;
+        private System.Windows.Forms.Label lblLoginBOX;
+        private System.Windows.Forms.Label lblAlert;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Button butClear;
+        private System.Windows.Forms.Button butLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblForgotPass;
+    }
+}
+
